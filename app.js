a336445a6fd37e1036860386f8faef1836056591
JoxWunderwaffe <31460416+JoxWunderwaffe@users.noreply.github.com>
2018-06-09 20:47:34 -0500

    fix1 undo

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("firebase-info.json");
+const serviceAccount = require("./firebase-info.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
    credential: admin.credential.cert(serviceAccount),
-   databaseURL: "./https://joxmad1.firebaseio.com/"
+   databaseURL: "https://joxmad1.firebaseio.com/"
  });
 
 
