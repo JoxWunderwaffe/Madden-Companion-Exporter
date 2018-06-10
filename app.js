5cf2886bd9f9fadb6b89c007484abfd2ae0fc0c9
JoxWunderwaffe <31460416+JoxWunderwaffe@users.noreply.github.com>
2018-06-09 21:36:16 -0500

    const to var - firebase

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("./firebase-info.json");
+var serviceAccount = require("./firebase-info.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
